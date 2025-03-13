43a50c665bcf6181b14b438577d003283bf7af91
Sourabh Dattaram Sakpal <90976821+SourabhSakpal93@users.noreply.github.com>
2025-03-13 19:26:32 +0530

    Sum of two numbers

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	 int num1,num2;
+		System.out.println("Enter twoo numbers  ");
+		Scanner sc =  new Scanner(System.in);
+	    System.out.println("Enter num1  numbers : ");
+		num1 = sc.nextInt();
+	    System.out.println("Enter num2 numbers : ");
+	    num2 = sc.nextInt();
+	    
+	    System.out.println("Sum of two number are :"+(num1+num2));
+		
+	}
+}
